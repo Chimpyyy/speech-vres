db2f1d21edb1ba26d1bc7e145eea77d3afbe7fdb
JLRinon-tip <qjlqrinon@tip.edu.ph>
2024-01-26 11:03:40 +0800

    Cone snap point  (11:03AM 01/26 )

--- Assets/Scripts/SnapToPosition.cs
+++ Assets/Scripts/SnapToPosition.cs
@@ -1,22 +1,16 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
 public class SnapToPosition : MonoBehaviour
 {
     public Transform[] snapPoints;
-    private GameObject indicatorInstance;
     public float snapDistance = 0.1f;
-
-    void Start()
-    {
-    
-    }
 
     void Update()
     {
         foreach (Transform snapPoint in snapPoints)
         {
-            float distance = Vector3.Distance(transform.position, snapPoint.position);
-
-            if (distance < snapDistance)
+            if (Vector3.Distance(transform.position, snapPoint.position) < snapDistance)
             {
                 SnapObject(snapPoint);
                 break;
@@ -27,15 +21,37 @@ public class SnapToPosition : MonoBehaviour
     void SnapObject(Transform snapPoint)
     {
         transform.position = snapPoint.position;
+        transform.rotation = snapPoint.rotation;
+
+        DisableRenderer(snapPoint);
+        DisableGrabInteractable();
+        DisableRigidbody();
+    }
+
+    void DisableRenderer(Transform snapPoint)
+    {
         Renderer snapPointRenderer = snapPoint.GetComponent<Renderer>();
         if (snapPointRenderer != null)
         {
             snapPointRenderer.enabled = false;
         }
+    }
+
+    void DisableGrabInteractable()
+    {
+        XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
+        if (grabInteractable != null)
+        {
+            grabInteractable.enabled = false;
+        }
+    }
+
+    void DisableRigidbody()
+    {
         Rigidbody rb = GetComponent<Rigidbody>();
         if (rb != null)
         {
-            rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+            rb.isKinematic = true;
         }
     }
 }
