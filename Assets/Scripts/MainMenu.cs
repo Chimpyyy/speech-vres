405b1234cb81de66b800668370658c2ea815e28b
JLRinon <rinon191@gmail.com>
2024-02-08 19:23:23 +0800

    7:22 PM Thursday, February 8, 2024
    Patient Turned on his side

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    //{FOR MAIN MENU --START--}
     public void StartSimulation()
     {
         SceneManager.LoadSceneAsync(1);
     }
+    public void QuitSimulation()
+    {
+        Application.Quit();
+    }
+    //{FOR MAIN MENU --END--}
+
+
+
+    //{FOR PATIENT ASSESSMENT --START--}
     public void StartPatientassessment()
     {
         SceneManager.LoadSceneAsync(2);
@@ -17,22 +27,26 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(3);
     }
-    public void StartPatientassessment3()
+    public void StartPatientassessment3and4()
     {
         SceneManager.LoadSceneAsync(4);
     }
+    public void StartPatientassessment5()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }
+    public void StartPatientassessment6()
+    {
+        SceneManager.LoadSceneAsync(6);
+    }
+    //{FOR PATIENT ASSESSMENT --END--}
+
+
+
+    //{FOR RAPID EXTRICATION --START--}
     public void StartRapidExtrication1()
     {
         SceneManager.LoadSceneAsync(5);
     }
-
-    public void StartPatientassessment4()
-    {
-        SceneManager.LoadSceneAsync(5);
-    }
-
-    public void QuitSimulation()
-    {
-        Application.Quit();
-    }
+    //{FOR RAPID EXTRICATION --END--}
 }
